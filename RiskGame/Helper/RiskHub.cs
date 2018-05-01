3db542a79535aecbaad304394cde28d79ed5e266
patompoltsj@gmail.com <patompoltsj@gmail.com>
2018-05-01 18:49:07 +0700

    update

---
+++ RiskGame/Helper/RiskHub.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace RiskGame.Helper
+{
+    public class RiskHub : Hub
+    {
+        private static string conString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ToString();
+
+        public void Hello()
+        {
+            Clients.All.hello();
+        }
+
+        [HubMethodName("sendMessages")]
+        public static void SendMessages()
+        {
+            IHubContext context = GlobalHost.ConnectionManager.GetHubContext<RiskHub>();
+            context.Clients.All.updateMessages();
+        }
+
+    }
+}
